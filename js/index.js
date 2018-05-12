93061fb7f979d2eab901c44e2b5cbd4d5590a7fa
evox1994 <dmitrybatukhtin@gmail.com>
2018-05-12 14:28:52 +0700

    sdfsafd

--- js/index.js
+++ js/index.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
 	$('.reviews-slider').slick({
 		infinite: true,
 		slidesToShow: 2,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+				}
+			}
+		]
 	});
 	
 });
