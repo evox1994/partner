b851f200c1972165ccd90df1b10e797be4e52f0f
evox1994 <dmitrybatukhtin@gmail.com>
2018-05-13 17:27:39 +0700

    ваымывамывам

--- js/main.js
+++ js/main.js
@@ -8,4 +8,57 @@ $(document).ready(function(){
         }
     });
 
+    function Add() {
+        $('.header').addClass("active");
+    }
+    function Remove() {
+        $('.header').removeClass("active");
+    }
+
+    $('.mobile-btn').click(function(){
+        var st = $(window).scrollTop();
+        if ( $(this).hasClass("active") ) {
+            $(this).removeClass("active");
+            $('.header-nav').animate({height: 0},300);
+            setTimeout(function(){
+                $('.header-nav').removeClass("active");
+                $('.header').removeClass("menu-active");
+            },300);
+        } else {
+            $(this).addClass("active");
+            $('.header-nav').addClass("active");
+            $('.header').addClass("menu-active");
+            $('.header-nav').animate({height: 260},300);
+        }
+    });
+
+    $(window).scroll(function(){
+        var st = $(this).scrollTop();
+        if ( $(window).width() > 768 ) {
+            if ( st > 117 ) {
+                Add();
+            } else {
+                Remove();
+            }
+        } else {
+            if ( st > 0 ) {
+                Add();
+            } else {
+                Remove();
+            }
+        }
+    });
+
+    $(window).resize(function(){
+        if ( $(this).width() > 768 ) {
+            $('.header-nav').height('auto');
+        } else {
+            if ( $('.mobile-btn').hasClass("active") ) {
+                $('.header-nav').height(260);
+            } else {
+                $('.header-nav').height(0);
+            }
+        }
+    });
+
 });
